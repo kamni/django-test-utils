a25546ac7cc82de0fcb9dc53c0792797b6c67134
Jay Leadbetter <jay.leadbetter@gmail.com>
2014-02-11 08:31:39 -0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+import os
+from setuptools import setup, find_packages
+
+ROOT = os.path.abspath(os.path.dirname(__file__))
+
+base_config = {'name': 'django_test_utils',
+               'version': '0.0.1',
+               'description': 'A collection of utilities for testing Django apps.',
+               'long_description': open(os.path.join(ROOT, 'README.md')).read(),
+               'author': 'J Leadbetter',
+               'author_email': 'codemonkey@jleadbetter.com',
+               'url': 'http://bitbucket.org/kamni/django-test-utils',
+               'license': 'Affero GPL v3',
+               'packages': find_packages(),
+               'include_package_data': True,
+               'zip_safe': False,
+               'install_requires': ['Django>=1.5'],
+               'classifiers': ['Development Status :: 2 - Pre-Alpha',
+                               'Environment :: Web Environment',
+                               'Framework :: Django',
+                               'Intended Audience :: Developers',
+                               'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+                               'Programming Language :: Python :: 2 :: Only']
+              }
+
+setup(**base_config)
